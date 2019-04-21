e0ff94a6c68020bbc0f9f49dd4d3b4587632ab13
Piotr <ptrysinski@gmail.com>
2019-04-21 22:04:06 +0200

    skeleton created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { connect } from 'react-redux'
 
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    )
-  }
+const App = () => {
+	return (
+		<div>
+		</div>
+	)
 }
 
-export default App
+const mapStateToProps = state => ({
+
+})
+
+const mapDispatchToProps = dispatch => ({
+	
+})
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps,
+)(App)
